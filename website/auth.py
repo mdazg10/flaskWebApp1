464c7f027326bc3e3d407c8196c57373e5e1c7b7
mdazg10 <107653481+mdazg10@users.noreply.github.com107653481+mdazg10@users.noreply.github.com>
2024-08-22 19:54:31 -0500

    auth update 1

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,27 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+
+@auth.route('/login', method=['GET', 'POST'])
 def login():
     return render_template("login.html")
+
+
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+
+
+@auth.route('/sign-up', method=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
     return render_template("sign_up.html")
