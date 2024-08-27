2376e4743daffb3185fdd4660a2faa20adeaa5a7
mdazg10 <107653481+mdazg10@users.noreply.github.com107653481+mdazg10@users.noreply.github.com>
2024-08-26 20:19:51 -0500

    auth base sign_up updated / flash message base

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -23,5 +23,18 @@ def sign_up():
         firstName = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 character', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters', category='error')
+        else:
+            # add user to database
+            flash('Account created!')
+            
 
     return render_template("sign_up.html")
